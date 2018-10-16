95b37e3245e6c16bc7caa1062cb435b75fc7d8a6
Hamza Assyad <hamzaad@amazon.com>
2018-10-16 10:48:19 -0700

    Splitting the CognitoUserStore into multiple files and setting it up as a partial class

---
+++ src/Amazon.AspNetCore.Identity.AWSCognito/CognitoUserStore/CognitoUserStore.IUserRoleStore.cs
@@ -0,0 +1,147 @@
+﻿/*
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Amazon.AspNetCore.Identity.AWSCognito
+{
+    public partial class CognitoUserStore<TUser> : IUserRoleStore<TUser> where TUser : CognitoUser
+    {
+        #region IUserStore
+
+        /// <summary>
+        /// Finds and returns a user, if any, who has the specified <paramref name="userId"/>.
+        /// </summary>
+        /// <param name="userId">The user ID to search for.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to propagate notifications that the operation should be canceled.</param>
+        /// <returns>
+        /// The <see cref="Task"/> that represents the asynchronous operation, containing the user matching the specified <paramref name="userId"/> if it exists.
+        /// </returns>
+        public async Task<TUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            // The userId can be the userName, the email or the phone number depending on the User Pool login policy
+            var user = await _pool.FindByIdAsync(userId).ConfigureAwait(false);
+            return user as TUser;
+        }
+
+        /// <summary>
+        /// Returns the userId associated with the <paramref name="user"/>.
+        /// </summary>
+        /// <param name="user">The user to retrieve the id for.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to propagate notifications that the operation should be canceled.</param>
+        /// <returns>
+        /// The <see cref="Task"/> that represents the asynchronous operation, containing the userId belonging to the matching the specified <paramref name="userId"/>.
+        /// </returns>
+        public Task<string> GetUserIdAsync(TUser user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return Task.FromResult(user.UserID);
+        }
+
+        /// <summary>
+        /// Returns the UserName associated with the <paramref name="user"/>.
+        /// </summary>
+        /// <param name="user">The user to retrieve the UserName for.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to propagate notifications that the operation should be canceled.</param>
+        /// <returns>
+        /// The <see cref="Task"/> that represents the asynchronous operation, containing the UserName belonging to the matching the specified <paramref name="userId"/>.
+        /// </returns>
+        public Task<string> GetUserNameAsync(TUser user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return Task.FromResult(user.Username);
+        }
+
+        public Task<IdentityResult> CreateAsync(TUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> DeleteAsync(TUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            // Cognito is case-sensitive and does not support normalized user name.
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetNormalizedUserNameAsync(TUser user, CancellationToken cancellationToken)
+        {
+            // Cognito is case-sensitive and does not support normalized user name.
+            throw new NotImplementedException();
+        }
+
+        public Task SetNormalizedUserNameAsync(TUser user, string normalizedName, CancellationToken cancellationToken)
+        {
+            // Cognito is case-sensitive and does not support normalized user name.
+            throw new NotImplementedException();
+        }
+
+        public Task SetUserNameAsync(TUser user, string userName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> UpdateAsync(TUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IUserRoleStore
+
+        // Roles Handling:
+        public Task AddToRoleAsync(TUser user, string roleName, CancellationToken cancellationToken)
+        {
+            //WIP: AdminAddUserToGroupAsync
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveFromRoleAsync(TUser user, string roleName, CancellationToken cancellationToken)
+        {
+            //WIP: AdminRemoveUserFromGroupAsync
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<string>> GetRolesAsync(TUser user, CancellationToken cancellationToken)
+        {
+            //WIP: ListGroupsAsync
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsInRoleAsync(TUser user, string roleName, CancellationToken cancellationToken)
+        {
+            //WIP: AdminListGroupsForUserAsync
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<TUser>> GetUsersInRoleAsync(string roleName, CancellationToken cancellationToken)
+        {
+            //WIP: ListUsersInGroupAsync
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
