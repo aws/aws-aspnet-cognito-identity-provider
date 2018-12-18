0a59e8cf6e0089b7f0bfa85eef4841df151c13d4
Hamza Assyad <hamzaad@amazon.com>
2018-12-18 13:40:04 -0800

    Adding a sample project with controllers

---
+++ samples/Samples/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,93 @@
+﻿using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace Samples.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly CognitoUserManager<CognitoUser> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly CognitoUserPool _pool;
+
+        public RegisterModel(
+            UserManager<CognitoUser> userManager,
+            SignInManager<CognitoUser> signInManager,
+            ILogger<RegisterModel> logger,
+            CognitoUserPool pool)
+        {
+            _userManager = userManager as CognitoUserManager<CognitoUser>;
+            _signInManager = signInManager;
+            _logger = logger;
+            _pool = pool;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [Display(Name = "UserName")]
+            public string UserName { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        public void OnGet(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var user = _pool.GetUser(Input.UserName);
+                user.Attributes.Add(CognitoAttributesConstants.Email, Input.Email);
+
+                var result = await _userManager.CreateAsync(user, Input.Password).ConfigureAwait(false);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    await _signInManager.SignInAsync(user, isPersistent: false).ConfigureAwait(false);
+
+                    return RedirectToPage("./ConfirmAccount");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
+}
