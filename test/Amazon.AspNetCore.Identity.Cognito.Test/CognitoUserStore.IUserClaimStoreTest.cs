4b6c5dba95becfaf58792e93245c1f1fe058ad78
Hamza Assyad <hamzaad@amazon.com>
2019-01-30 16:10:10 -0800

    Various stores unit test coverage

---
+++ test/Amazon.AspNetCore.Identity.Cognito.Test/CognitoUserStore.IUserClaimStoreTest.cs
@@ -0,0 +1,120 @@
+﻿/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+using Amazon.CognitoIdentityProvider.Model;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Amazon.AspNetCore.Identity.Cognito.Test
+{
+    public partial class CognitoUserStoreTest
+    {
+        private AdminGetUserResponse GET_USER_RESPONSE = new AdminGetUserResponse
+        {
+            UserAttributes = new List<AttributeType>()
+                {
+                    new AttributeType()
+                    {
+                        Name = "Name1",
+                        Value = "Value1"
+                    },
+                    new AttributeType()
+                    {
+                        Name = "Name2",
+                        Value = "Value2"
+                    },
+                    new AttributeType()
+                    {
+                        Name = "Name2",
+                        Value = "Value2"
+                    }
+                }
+        };
+
+        [Fact]
+        public async void Test_GivenAUser_WhenGetClaims_ThenTheListOfClaimsIsRetrieved()
+        {
+            _cognitoClientMock.Setup(mock => mock.AdminGetUserAsync(It.IsAny<AdminGetUserRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(GET_USER_RESPONSE)).Verifiable();
+            var output = await _store.GetClaimsAsync(_userMock.Object, CancellationToken.None).ConfigureAwait(false);
+            Assert.Equal(GET_USER_RESPONSE.UserAttributes.Count, output.Count);
+            Assert.Equal(GET_USER_RESPONSE.UserAttributes[2].Name, output[2].Type);
+            Assert.Equal(GET_USER_RESPONSE.UserAttributes[1].Value, output[1].Value);
+            _cognitoClientMock.Verify();
+        }
+
+        [Fact]
+        public async void Test_GivenAUser_WhenAddClaimsWithPopulatedListOfClaims_ThenAdminUpdateUserAttributesIsCalled()
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim("Name1", "Value1")
+            };
+            _cognitoClientMock.Setup(mock => mock.AdminUpdateUserAttributesAsync(It.IsAny<AdminUpdateUserAttributesRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(new AdminUpdateUserAttributesResponse())).Verifiable();
+            await _store.AddClaimsAsync(_userMock.Object, claims, CancellationToken.None).ConfigureAwait(false);
+            _cognitoClientMock.Verify();
+        }
+
+        [Fact]
+        public async void Test_GivenAUser_WhenAddClaimsWithEmptyListOfClaims_ThenAdminUpdateUserAttributesIsNeverCalled()
+        {
+            var claims = new List<Claim>();
+            await _store.AddClaimsAsync(_userMock.Object, claims, CancellationToken.None).ConfigureAwait(false);
+            _cognitoClientMock.Verify(mock => mock.AdminUpdateUserAttributesAsync(It.IsAny<AdminUpdateUserAttributesRequest>(), It.IsAny<CancellationToken>()), Times.Never);
+        }
+
+        [Fact]
+        public async void Test_GivenAUser_WhenRemoveClaimsWithAValidClaim_ThenAdminDeleteUserAttributesIsCalled()
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim("Name1", "Value1")
+            };
+            _cognitoClientMock.Setup(mock => mock.AdminGetUserAsync(It.IsAny<AdminGetUserRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(GET_USER_RESPONSE)).Verifiable();
+            _cognitoClientMock.Setup(mock => mock.AdminDeleteUserAttributesAsync(It.IsAny<AdminDeleteUserAttributesRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(new AdminDeleteUserAttributesResponse())).Verifiable();
+            await _store.RemoveClaimsAsync(_userMock.Object, claims, CancellationToken.None).ConfigureAwait(false);
+            _cognitoClientMock.Verify();
+        }
+
+        [Fact]
+        public async void Test_GivenAUser_WhenRemoveClaimsWithAnInvalidClaim_ThenAdminDeleteUserAttributesIsNeverCalled()
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim("Unknown", "Unknown")
+            };
+            _cognitoClientMock.Setup(mock => mock.AdminGetUserAsync(It.IsAny<AdminGetUserRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(GET_USER_RESPONSE)).Verifiable();
+            _cognitoClientMock.Setup(mock => mock.AdminDeleteUserAttributesAsync(It.IsAny<AdminDeleteUserAttributesRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(new AdminDeleteUserAttributesResponse())).Verifiable();
+            await _store.RemoveClaimsAsync(_userMock.Object, claims, CancellationToken.None).ConfigureAwait(false);
+            _cognitoClientMock.Verify(mock => mock.AdminDeleteUserAttributesAsync(It.IsAny<AdminDeleteUserAttributesRequest>(), It.IsAny<CancellationToken>()), Times.Never);
+        }
+
+        [Fact]
+        public async void Test_GivenAUser_WhenReplaceClaim_ThenThrowsANotSupportedException()
+        {
+            await Assert.ThrowsAsync<NotSupportedException>(() => _store.ReplaceClaimAsync(null, null, null, CancellationToken.None)).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async void Test_GivenAClaim_WhenGetUsersForClaim_ThenThrowsANotSupportedException()
+        {
+            await Assert.ThrowsAsync<NotSupportedException>(() => _store.GetUsersForClaimAsync(null, CancellationToken.None)).ConfigureAwait(false);
+        }
+    }
+}
---
+++ test/Amazon.AspNetCore.Identity.Cognito.Test/CognitoUserStore.IUserTwoFactorStoreTest.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+using Amazon.CognitoIdentityProvider.Model;
+using Moq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Amazon.AspNetCore.Identity.Cognito.Test
+{
+    public partial class CognitoUserStoreTest
+    {
+        [Fact]
+        public async void Test_GivenAUser_WhenGetTwoFactorEnabledWithAnMFAOption_ThenTheResponseIsTrue()
+        {
+            var response = new AdminGetUserResponse()
+            {
+                MFAOptions = new List<MFAOptionType>()
+                {
+                    new MFAOptionType()
+                }
+            };
+            _cognitoClientMock.Setup(mock => mock.AdminGetUserAsync(It.IsAny<AdminGetUserRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(response)).Verifiable();
+            var output = await _store.GetTwoFactorEnabledAsync(_userMock.Object, CancellationToken.None).ConfigureAwait(false);
+            Assert.True(output);
+            _cognitoClientMock.Verify();
+        }
+
+        [Fact]
+        public async void Test_GivenAUser_WhenGetTwoFactorEnabledWithoutAnMFAOption_ThenTheResponseIsFalse()
+        {
+            var response = new AdminGetUserResponse()
+            {
+                MFAOptions = new List<MFAOptionType>()
+            };
+            _cognitoClientMock.Setup(mock => mock.AdminGetUserAsync(It.IsAny<AdminGetUserRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(response)).Verifiable();
+            var output = await _store.GetTwoFactorEnabledAsync(_userMock.Object, CancellationToken.None).ConfigureAwait(false);
+            Assert.False(output);
+            _cognitoClientMock.Verify();
+        }
+
+        [Fact]
+        public async void Test_GivenAUser_WhenSetTwoFactorEnabled_ThenAdminSetUserSettingsAsyncIsCalled()
+        {
+            _cognitoClientMock.Setup(mock => mock.AdminSetUserSettingsAsync(It.IsAny<AdminSetUserSettingsRequest>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(new AdminSetUserSettingsResponse())).Verifiable();
+            await _store.SetTwoFactorEnabledAsync(_userMock.Object, false, CancellationToken.None).ConfigureAwait(false);
+            _cognitoClientMock.Verify();
+        }
+    }
+}
